50f284026d457398583c31c237902b69035014d0
bahuljain <bkj2111@columbia.edu>
2016-01-17 11:37:27 -0500

    adding bst

---
+++ OCaml - Université Paris Diderot/week 3/binary_search_tree.ml
@@ -0,0 +1,44 @@
+type 'a bst = 
+  | Empty
+  | Node of 'a bst * 'a * 'a bst
+;;
+
+(* Lovely part is you can see the whole damn tree at the end of the result :) *)
+let rec insert x = function
+  | Empty -> Node (Empty, x, Empty)
+  | Node (l, y, r) ->
+    if x = y then Node (l, y, r)
+    else if x < y then Node (insert x l, y, r)
+    else Node (l, y, insert x r)
+;;
+
+let rec find_max = function
+  | Empty -> assert false
+  | Node (_, x, Empty) -> x
+  | Node (_, x, r) -> find_max r
+;;
+
+let rec delete x = function 
+  | Empty -> Empty
+  | Node (l, y, r) ->
+    if x = y then 
+      match l, r with
+        | Empty, r -> r
+        | l, Empty -> l
+        | l,r -> let m = find_max l in Node (delete m l, m, r)
+    else if x < y then Node (delete x l, y, r)
+    else  Node (l, y, delete x r)
+;;
+
+let balanced root : bool = 
+  let rec height = function
+  | Empty -> 0
+  | Node (l,_,r) ->
+    let left = height l and right = height r in
+    if left = -1 || right = -1 then -1
+    else if abs (left - right) > 1 then -1
+    else 1 + (max left right)
+  in
+  if height root = -1 then false
+  else true
+;;
